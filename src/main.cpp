26d8817280291fcd6b9cbb48fc8cee8fea5a3b87
Varley Silva <varleysilva@me.com>
2016-04-24 15:38:56 -0300

    adiçao de uma tela de selecao do tipo de imagem que deseja desencriptar

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,24 @@ using namespace std;
 
 int main(){
   string nomeDaImagem;
+  double selecionaFormatoDaImagem;
+  cout << "Digite 1 para desencriptar imagens .PPM" << endl;
+  cout << "Digite 2 para desencriptar imagens .PGM" << endl;
+  cin >> selecionaFormatoDaImagem;
+
+  if(selecionaFormatoDaImagem == 1){
 
   DecifraPpm * decifraPPM;
   decifraPPM = new DecifraPpm();
   cout << "Digite o nome da imagem: " << endl;
   cin >> nomeDaImagem;
   decifraPPM->openImage(nomeDaImagem);
-
   decifraPPM->decode();
-
+  }
+  else if (selecionaFormatoDaImagem == 2){
+    cout << "falta implementar" << endl;
+  }
+  else if (selecionaFormatoDaImagem != 1 && selecionaFormatoDaImagem != 2){
+    cout << "Opção inválida! Tente novamente e selecione 1 ou 2." << endl;
+  }
 }
