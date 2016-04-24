67912fb0c70dbc2fe41f7949ff8fa91572e8b706
Varley Silva <varleysilva@me.com>
2016-04-24 18:58:22 -0300

    adiçao dos filtros azul e verde no metodo decode da PPM

--- src/decifrappm.cpp
+++ src/decifrappm.cpp
@@ -24,7 +24,16 @@ void DecifraPpm::decode(){
       }
   }
   file.close();
-  //aplicando o filtro
+
+  double selecionaFiltro;
+  cout << "Selecione o filtro que quer aplicar:" << endl;
+  cout << "1 - Filtro VERMELHO" << endl;
+  cout << "2 - Filtro VERDE" << endl;
+  cout << "3 - Filtro AZUL" << endl;
+  cout << "0 - CANCELAR" << endl;
+  cin >> selecionaFiltro;
+  if (selecionaFiltro == 1){
+  //aplicando o filtro VERMELHO
   char filtro;
   for(row = 0; row < height; row++){
       for(col=0; col < width; col++){
@@ -33,7 +42,32 @@ void DecifraPpm::decode(){
           pixel_value[row][col][2] = filtro; //azul
       }
   }
-
+} else if (selecionaFiltro == 2){
+  //aplicando o filtro VERDE
+  char filtro;
+  for(row = 0; row < height; row++){
+      for(col=0; col < width; col++){
+          filtro = pixel_value[row][col][1];//verde
+          pixel_value[row][col][0] = filtro; //vermelho
+          pixel_value[row][col][2] = filtro; //azul
+      }
+  }
+} else if (selecionaFiltro == 3){
+  //aplicando o filtro azul
+  char filtro;
+  for(row = 0; row < height; row++){
+      for(col=0; col < width; col++){
+        filtro = pixel_value[row][col][2]; //azul
+        pixel_value[row][col][0] = filtro; //vermelho
+        pixel_value[row][col][1] = filtro; //verde
+      }
+  }
+}else if (selecionaFiltro == 0) {
+  exit(0);
+}else if (selecionaFiltro != 1 && selecionaFiltro != 2 && selecionaFiltro != 3){
+  cout << "Opção inválida!" << endl;
+  exit(1);
+}
 
   fstream outFile;
   string nomeDeSaida;
